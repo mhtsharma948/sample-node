0125e06d1668a7da65058fe325dd243b060332d3
mhtsharma948 <mhtsharma948@gmail.com>
2017-11-16 12:19:55 +0530

    Added Travis

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express')
 var router = express.Router()
 var os = require('os')
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express !!! host:' + os.hostname() })
+    /* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express !!! host:' + os.hostname() })
+
+})
+
+router.get('/user', (req, res, next) => {
+    let data = {
+        "firstName": "Mohit",
+        "lastName": "Sharma",
+    }
+    res.end(JSON.stringify(data));
 })
 
 module.exports = router
